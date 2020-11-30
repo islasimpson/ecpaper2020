3bb56626d863b57bdffb35d07b2662b3bc4f3e05
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2020-11-29 20:22:35 -0700

    adding the option to have latitudes going from 90N to 90S when spatially averaging

--- ecpaper_utils/averaging_utils.py
+++ ecpaper_utils/averaging_utils.py
@@ -5,6 +5,12 @@ def cosweightlonlat(darray,lon1,lon2,lat1,lat2):
     """Calculate the weighted average for an [:,lat,lon] array over the region
     lon1 to lon2, and lat1 to lat2
     """
+    # flip latitudes if they are decreasing
+    if (darray.lat[0] > darray.lat[darray.lat.size -1]):
+        print("flipping latitudes")
+        darray = darray.sortby('lat')
+
+
     region=darray.sel(lon=slice(lon1,lon2),lat=slice(lat1,lat2))
     weights = np.cos(np.deg2rad(region.lat))
     regionw = region.weighted(weights)
