b77bae06a85cb7ba750eb4e7424d1699a04d9881
Isla Simpson <islas@andre.cgd.ucar.edu>
2020-12-19 13:17:05 -0700

    adding in function to averaging_utils to calculate the weighed average of a zonal mean

--- ecpaper_utils/averaging_utils.py
+++ ecpaper_utils/averaging_utils.py
@@ -23,3 +23,20 @@ def cosweightlonlat(darray,lon1,lon2,lat1,lat2):
     regionm = regionw.mean(("lon","lat"))
 
     return regionm
+
+def cosweightlat(darray, lat1, lat2):
+    """Calculate the weighted average for an [:,lat] array over the region
+    lat1 to lat2
+    """
+
+    # flip latitudes if they are decreasing
+    if (darray.lat[0] > darray.lat[darray.lat.size -1]):
+        print("flipping latitudes")
+        darray = darray.sortby('lat')
+
+    region = darray.sel(lag=slice(lat1, lat2))
+    weights=np.cos(np.deg2rad(region.lat))
+    regionw = region.weighted(weights)
+    regionm = regionw.mean("lat")
+
+    return regionm
